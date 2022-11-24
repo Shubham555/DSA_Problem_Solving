964ac31390c65e52684887e8e6acbadfde6d7667
Shubham555 <118034649+Shubham555@users.noreply.github.com>
2022-11-25 01:14:06 +0530

    Time: 91 ms (10.39%), Space: 25.9 MB (54.32%) - LeetHub

---
+++ 1720-decode-xored-array/1720-decode-xored-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+        vector<int> arr; 
+      //  arr[0] = first;
+        arr.push_back(first);
+        for(int i=0;i<encoded.size();i++){
+            arr.push_back(encoded[i]^arr[i]);
+        }
+        return arr;
+    }
+};
+
+/* public int[] decode(int[] A, int first) {
+        int n = A.length, res[] = new int[n + 1];
+        res[0] = first;
+        for (int i = 0; i < n; ++i)
+            res[i + 1] = res[i] ^ A[i];
+        return res;
+    }*/
