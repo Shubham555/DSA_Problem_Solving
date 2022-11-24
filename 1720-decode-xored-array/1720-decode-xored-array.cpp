7aa901c23ecb6a4062b29f1030efcf95645e3449
Shubham555 <118034649+Shubham555@users.noreply.github.com>
2022-11-25 01:19:01 +0530

    Time: 87 ms (17.43%), Space: 26.1 MB (34.37%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.cpp
+++ 1720-decode-xored-array/1720-decode-xored-array.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     vector<int> decode(vector<int>& encoded, int first) {
-        vector<int> arr; 
-      //  arr[0] = first;
+        vector<int> arr;  
         arr.push_back(first);
+        
         for(int i=0;i<encoded.size();i++){
             arr.push_back(encoded[i]^arr[i]);
         }
         return arr;
     }
 };
+
+// java solution
 
 /* public int[] decode(int[] A, int first) {
         int n = A.length, res[] = new int[n + 1];
