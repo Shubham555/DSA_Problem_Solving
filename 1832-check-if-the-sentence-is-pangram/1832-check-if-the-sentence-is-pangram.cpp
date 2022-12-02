011dc1d5d4de26567f39fbce78024a34b5423d0d
Shubham555 <118034649+Shubham555@users.noreply.github.com>
2022-12-02 09:29:14 +0530

    Time: 10 ms (12.88%), Space: 6.5 MB (46.77%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        set<int> s;
+        for(auto &c : sentence){
+            s.insert(c);
+        }
+        return s.size()==26;
+    }
+};
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
