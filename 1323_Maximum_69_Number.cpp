48faa6ac774b6b650e5b387e397367018d2e3719
Shubham555 <118034649+Shubham555@users.noreply.github.com>
2022-12-16 11:15:10 +0530

    Create 1323_Maximum_69_Number.cpp

---
+++ 1323_Maximum_69_Number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string ans = to_string(num);
+
+        for(int i=0;i<ans.size();i++){
+            if(ans[i]=='6'){
+               ans[i] = '9';
+               break;
+            }
+        }
+        return stoi(ans);
+    }
+};
