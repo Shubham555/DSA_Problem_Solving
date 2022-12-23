981a75e0fea2fe5105c6b5ca86046284df48ed0b
Shubham555 <118034649+Shubham555@users.noreply.github.com>
2022-12-23 13:15:14 +0530

    Create 724_Find_Pivot_Index.cpp

---
+++ 724_Find_Pivot_Index.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+
+        int leftSum = 0;
+        int RightSum =0;
+        int totalSum =0;
+        for(int i:nums){
+            totalSum = totalSum + i;
+        }
+        for(int i=0;i<nums.size();i++){
+            RightSum = totalSum - nums[i] - leftSum;
+            if(RightSum == leftSum ){
+                return i;
+            }
+            else{
+             leftSum += nums[i];   
+            }
+        }
+        return -1;
+        
+    }
+};
