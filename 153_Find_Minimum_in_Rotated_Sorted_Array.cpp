c78f1f03bfd332d34c64ce1c54b3043e0f781137
Shubham555 <118034649+Shubham555@users.noreply.github.com>
2022-12-27 00:07:43 +0530

    Create 153_Find_Minimum_in_Rotated_Sorted_Array.cpp

---
+++ 153_Find_Minimum_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+
+         int s = 0;
+         int e = nums.size()-1;
+         int mid = s + (e-s)/2;
+
+         while(s<e){
+             if(nums[mid]>=nums[e]){
+                 s = mid +1;
+             }
+             else{
+                 e = mid;
+             }
+             mid = s + (e-s)/2; 
+         }
+         return nums[s]; //or we can return 'e';as both are lying on same index(s=e)then onlytheloop exits
+     }
+};
