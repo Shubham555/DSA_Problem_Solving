b884375c4a15f558dbbafc0a0c33f3f6aed4c631
Shubham555 <118034649+Shubham555@users.noreply.github.com>
2023-01-04 21:11:15 +0530

    Update 498_Diagonal_Traverse.cpp

--- 498_Diagonal_Traverse.cpp
+++ 498_Diagonal_Traverse.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        if (m == 0) return vector<int>();
-        int n = matrix[0].size();
-        vector<vector<int>> tmp (m+n-1);
-        for (int i = 0; i < m+n-1 ; i++) {
-            int row = max(0, i-n+1);
-            int col = min(i, n-1);
-            for (; col >= 0 && row < m; row++, col--) {
-                tmp[i].push_back(matrix[row][col]);
-            }
-        }
-        vector<int> res;
-        for (int i = 0; i< tmp.size(); i++) {
-            if (i % 2) res.insert(res.end(), tmp[i].begin(), tmp[i].end());
-            else res.insert(res.end(), tmp[i].rbegin(), tmp[i].rend());
-        }
-        return res;
+       if(matrix.size()==0) 
+            return vector<int>();
+       int m = matrix.size(),  n = matrix[0].size(), r =0, c=0;
+       vector<int> result(m*n);
+
+       for(int i=0;i<result.size();i++){
+           result[i] = matrix[r][c];
+           if((r+c)%2==0){
+               if(c == n-1){
+                   r++;
+               }else if(r==0){
+                   c++;
+               }else{
+                   r--;
+                   c++;
+               }
+           }else{
+               if(r==m-1){
+                   c++;
+               }else if(c==0){
+                   r++;
+               }else{
+                   r++;
+                   c--;
+               }
+           }
+       }
+        return result;
     }
 };
