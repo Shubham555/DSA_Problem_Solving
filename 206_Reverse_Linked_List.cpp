bf8f3b72c09820d7560875af4a57ab1c401693dc
Shubham Gite <118034649+Shubham555@users.noreply.github.com>
2024-01-22 23:58:36 +0530

    Create 206_Reverse_Linked_List.cpp

---
+++ 206_Reverse_Linked_List.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *pre = new ListNode(0), *cur = head;
+        pre -> next = head;
+        while (cur && cur -> next) {
+            ListNode* temp = pre -> next;
+            pre -> next = cur -> next;
+            cur -> next = cur -> next -> next;
+            pre -> next -> next = temp;
+        }
+        return pre -> next;
+    }
+};
