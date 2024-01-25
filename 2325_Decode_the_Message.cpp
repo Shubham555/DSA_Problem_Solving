e46fc269a257b1d956f7eee9b0bd76b9af476718
Shubham Gite <118034649+Shubham555@users.noreply.github.com>
2024-01-26 00:54:49 +0530

    2325_Decode_the_Message.cpp

--- 2325_Decode_the_Message.cpp
+++ 2325_Decode_the_Message.cpp
@@ -1,1 +1,33 @@
-h
+class Solution {
+public:
+    string decodeMessage(string key, string message) {
+        map<char, char> mp;
+        int temp = 'a';
+        for (auto it : key) {
+            if (it != ' ' && mp.find(it) == mp.end()) {
+                mp[it] = temp++;
+                // cout<<it<<"->"<<mp[it]<<endl;
+            }
+        }
+
+        string decodedMessage = "";
+        for (auto it : message) {
+            if (it != ' ') {
+                decodedMessage += mp[it];
+            } else {
+                decodedMessage += ' ';
+            }
+        }
+        return decodedMessage;
+    }
+};
+
+// h-'a'
+// a-'b'
+// p-'c'
+// y-'d'
+// b-'e'
+// o-'f'
+//
+
+// mp['h']='a'
