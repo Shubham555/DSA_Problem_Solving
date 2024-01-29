a261a4244f38e8431f7b583232db473e0c452ae2
Shubham Gite <118034649+Shubham555@users.noreply.github.com>
2024-01-30 01:29:56 +0530

    Update 872_Leaf_Similar_Trees.cpp

--- 872_Leaf_Similar_Trees.cpp
+++ 872_Leaf_Similar_Trees.cpp
@@ -1,1 +1,38 @@
-hgf
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+void inorder(TreeNode* root, vector<int> &v){
+    // Base Case
+    if(root==NULL){
+        return;
+    }
+
+    inorder(root->left,v);
+    if(root->left == NULL && root->right == NULL){
+              v.push_back(root->val);
+        }
+    inorder(root->right,v);
+}
+class Solution {
+public:
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        vector<int> vec_1;
+        vector<int> vec_2;
+
+        inorder(root1,vec_1);
+        inorder(root2,vec_2);
+        if(vec_1==vec_2){
+            return true;
+        }
+            return false;
+    }
+};
