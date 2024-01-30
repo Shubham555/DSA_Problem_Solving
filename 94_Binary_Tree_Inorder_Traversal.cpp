96232f6d31936dcf8f579513d58f73096d0c1901
Shubham Gite <118034649+Shubham555@users.noreply.github.com>
2024-01-30 23:43:48 +0530

    Create 94_Binary_Tree_Inorder_Traversal.cpp

---
+++ 94_Binary_Tree_Inorder_Traversal.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    void dfs(TreeNode *node, vector<int> &inorder) {
+        if(node == NULL) return; 
+        
+        dfs(node->left, inorder); 
+        inorder.push_back(node->val); 
+        dfs(node->right, inorder); 
+    }
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> inorder;
+        dfs(root, inorder);
+        return inorder; 
+    }
+};
+
+
+// Iterative method
+/*
+class Solution {
+
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st; 
+        TreeNode* node = root;
+        vector<int> inorder; 
+        while(true) {
+            if(node != NULL) {
+                st.push(node); 
+                node = node->left; 
+            }
+            else {
+    
+                if(st.empty() == true) break; 
+                node = st.top(); 
+                st.pop(); 
+                inorder.push_back(node->val); 
+                node = node->right; 
+            }  
+        }
+        return inorder; 
+    }
+};
+*/
